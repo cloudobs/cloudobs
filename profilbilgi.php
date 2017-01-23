333dd83cefc3736640cbbff350710acf6242b628
Hasan Gökkaya <akinci35@gmail.com>
2017-01-23 16:52:18 +0200

    Update profilbilgi.php

--- profilbilgi.php
+++ profilbilgi.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="description" content="Uzman mı lazım? Hemen Bul">
   <meta name="description" content="Performans Analizi.">
   <meta name="keywords" content="finansal, operasyonel, personel, performans, analiz">
+  <meta name="author" content="Hasan GÖKKAYA">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
